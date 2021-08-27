5b4a661623966729b72cf311d8c2fdad946da2f4
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-08-27 19:02:09 +1200

    initial commit

--- GoalsAPI/urls.py
+++ GoalsAPI/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns+ = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
